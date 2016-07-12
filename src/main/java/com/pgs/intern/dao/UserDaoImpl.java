6fc17078b094afffb241cb4391151659e5e218fe
Kamil Michalik (PGS Software) <kmichalik@pgs-soft.com>
2016-07-12 12:44:52 +0200

    added class UserDaoIml.

---
+++ src/main/java/com/pgs/intern/dao/UserDaoImpl.java
@@ -0,0 +1,32 @@
+package com.pgs.intern.dao;
+
+import com.pgs.intern.models.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+/**
+ * Created by kmichalik on 7/12/2016.
+ */
+@Repository("UserDaoImpl")
+@Transactional(propagation = Propagation.REQUIRED)
+public class UserDaoImpl implements UserDao {
+
+    private EntityManager entityManager;
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public void setEntityManager() {
+        this.entityManager = entityManager;
+    }
+
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+
+}
