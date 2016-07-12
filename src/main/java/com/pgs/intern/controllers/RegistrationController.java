96f777334c07d46bccee8c52654b7b751db19b00
Lukasz Schiffer (PGS Software) <lschiffer@pgs-soft.com>
2016-07-12 13:12:05 +0200

    Added registration POST method.

--- src/main/java/com/pgs/intern/controllers/RegistrationController.java
+++ src/main/java/com/pgs/intern/controllers/RegistrationController.java
@@ -1,8 +1,14 @@
 package com.pgs.intern.controllers;
 
+import com.pgs.intern.models.RegistrationViewModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 /**
  * Created by Maciej Rosa on 7/12/2016 12:42 PM.
@@ -12,5 +18,15 @@ public class RegistrationController {
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String register(){
         return "unauthorised/register";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public RegistrationViewModel registerPost(@Valid @ModelAttribute RegistrationViewModel model, final BindingResult result) {
+        if (result.hasErrors()) {
+            return null;
+        }
+
+        return model;
     }
 }
