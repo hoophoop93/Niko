69663a70c474de2fa4fa03c91691e40d86c2c406
Lukasz Schiffer (PGS Software) <lschiffer@pgs-soft.com>
2016-07-12 10:59:20 +0200

    Added configuration for Postgres database.

--- src/main/java/com/pgs/intern/NikoApplication.java
+++ src/main/java/com/pgs/intern/NikoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NikoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NikoApplication.class, args);
 	}
