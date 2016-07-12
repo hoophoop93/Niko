bf1b96e288f87b4a75d6f48548d84d607c3570a8
Kamil Michalik (PGS Software) <kmichalik@pgs-soft.com>
2016-07-12 15:36:55 +0200

    PN-10 / PN-14  As a anonymous user I can register in application
    * Change class UserDaoImpl
    * Added new method

--- src/main/java/com/pgs/intern/dao/UserDaoImpl.java
+++ src/main/java/com/pgs/intern/dao/UserDaoImpl.java
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * Created by kmichalik on 7/12/2016.
@@ -30,5 +32,21 @@ public class UserDaoImpl implements UserDao {
         entityManager.persist(user);
     }
 
+    public boolean findByEmail(String email) {
+//        String queryString = "SELECT count(o.email) FROM User o where o.email = :email";
+        String queryString = "SELECT count(o.email) FROM User o where o.email = :email";
+        Query query = getEntityManager().createQuery(queryString);
+        query.setParameter("email", email);
+
+        List result = query.getResultList();
+        long firstElement = (long) result.get(0);
+
+        if (firstElement > 0) {
+            return true;
+        }else {
+            return false;
+        }
+
+    }
 
 }
