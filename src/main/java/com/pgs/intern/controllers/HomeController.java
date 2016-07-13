04362b96da8bbb5d0c68525cd8b3ab7c8340edd2
Lukasz Schiffer (PGS Software) <lschiffer@pgs-soft.com>
2016-07-13 10:11:02 +0200

    PN-10 / PN-16 Create controller for registration.
    *Added password hashing and password validation methods.

--- src/main/java/com/pgs/intern/controllers/HomeController.java
+++ src/main/java/com/pgs/intern/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.pgs.intern.controllers;
 
+import com.pgs.intern.utils.AccountUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,5 +20,20 @@ public class HomeController {
     @RequestMapping("/niko")
     public String niko() {
         return "authorised/index";
+    }
+
+    // For testing only; TODO REMOVE ME LATER
+    @ResponseBody
+    @RequestMapping("/test")
+    public String test() {
+        // Register
+        String test = "http://localhost:8080/register";
+        String hash = AccountUtils.getHashFor(test);
+
+        // From login form and db
+        String testIn = "http://localhost:8080/register";
+        String hashIn = hash;
+
+        return test + " -> " + hash + "<br/>" + testIn + " -> " + AccountUtils.validatePassword(testIn, hashIn);
     }
 }
