cf7922850c652dc559076d9ae6c9c6dd17f84489
Lukasz Schiffer (PGS Software) <lschiffer@pgs-soft.com>
2016-07-13 08:52:11 +0200

    PN-10 / PN-16 Create controller for registration.
    *Added file for utility for account and placeholder method for calculating hash.

---
+++ src/main/java/com/pgs/intern/utils/AccountUtils.java
@@ -0,0 +1,10 @@
+package com.pgs.intern.utils;
+
+/**
+ * Created by lschiffer on 7/13/2016.
+ */
+public class AccountUtils {
+    public static String getHashFor(String text) {
+        return "";
+    }
+}
