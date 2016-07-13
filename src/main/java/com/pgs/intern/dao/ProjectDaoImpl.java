6611477a9cf8d3e16e9358fbf75c4422e82cc997
Monika Zalucka (PGS Software) <mzalucka@pgs-soft.com>
2016-07-13 15:48:16 +0200

    PN-2 / PN-22 As a user I can create a new project
    * Created DAO class for project.
    * Implemented class DAO for project.

---
+++ src/main/java/com/pgs/intern/dao/ProjectDaoImpl.java
@@ -0,0 +1,34 @@
+package com.pgs.intern.dao;
+
+import com.pgs.intern.models.Project;
+import com.pgs.intern.models.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Created by mzalucka on 7/13/2016.
+ */
+
+    @Repository("ProjectDaoImpl")
+    @Transactional(propagation = Propagation.REQUIRED)
+    public class ProjectDaoImpl implements ProjectDao {
+
+        @PersistenceContext
+        private EntityManager entityManager;
+
+        public EntityManager getEntityManager() {
+            return entityManager;
+        }
+
+        public void setEntityManager() {
+            this.entityManager = entityManager;
+        }
+
+        public void save(Project project) {
+            entityManager.persist(project);
+        }
+}
