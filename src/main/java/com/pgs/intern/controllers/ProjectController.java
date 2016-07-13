44607f1987ba09589cbf350b4db665cc4613db4c
Maciej Rosa (PGS Software) <mrosa@pgs-soft.com>
2016-07-13 15:59:37 +0200

    PN-2 / PN-25 As a user I can create a new project.
    *Added post method.

--- src/main/java/com/pgs/intern/controllers/ProjectController.java
+++ src/main/java/com/pgs/intern/controllers/ProjectController.java
@@ -3,11 +3,15 @@ package com.pgs.intern.controllers;
 import com.pgs.intern.models.ProjectViewModel;
 import com.pgs.intern.services.CurrentUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 
 /**
  * Created by Maciej Rosa on 7/13/2016 3:26 PM.
@@ -31,5 +35,14 @@ public class ProjectController {
         modelAndView.addObject("model", new ProjectViewModel());
 
         return modelAndView;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/project/add", method = RequestMethod.POST)
+    public ProjectViewModel addProjectPost(@Valid @ModelAttribute ProjectViewModel projectViewModel,
+                                           final BindingResult result){
+        if(result.hasErrors())
+            return null;
+        return projectViewModel;
     }
 }
