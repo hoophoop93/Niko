200cbc9d3354117ff0d40ce762ad6266740ae122
Maciej Rosa (PGS Software) <mrosa@pgs-soft.com>
2016-07-14 14:11:51 +0200

    PN-6 / PN-31 As a project member I can report my feelings in a project for every day.
    * Added passing view model for mood
    * Created post method.

--- src/main/java/com/pgs/intern/controllers/MoodController.java
+++ src/main/java/com/pgs/intern/controllers/MoodController.java
@@ -1,9 +1,16 @@
 package com.pgs.intern.controllers;
 
+import com.pgs.intern.models.MoodViewModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 /**
  * Created by Maciej Rosa on 7/14/2016 1:48 PM.
@@ -14,7 +21,13 @@ public class MoodController {
 
     @RequestMapping(value = "/mood/add", method = RequestMethod.GET)
     public ModelAndView addMood(){
-        return new ModelAndView("authorised/moodadd"/* TODO: ,new MoodViewModel()*/);
+        return new ModelAndView("authorised/moodadd","model",new MoodViewModel());
+    }
+
+    @ResponseBody
+    public MoodViewModel addMoodPost(@Valid @ModelAttribute("model") MoodViewModel model,
+                                     final BindingResult result, final RedirectAttributes redirectAttributes){
+        return model;
     }
 
 }
