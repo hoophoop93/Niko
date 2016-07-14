7f9c1287b2716704ac3d5b0624184131c3e864a9
Monika Zalucka (PGS Software) <mzalucka@pgs-soft.com>
2016-07-14 12:47:25 +0200

    PN-1 / PN-18 As a user I can log in into application.
    *Added validation messages to annotations.

--- src/main/java/com/pgs/intern/models/LoginViewModel.java
+++ src/main/java/com/pgs/intern/models/LoginViewModel.java
@@ -1,5 +1,6 @@
 package com.pgs.intern.models;
 
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Size;
@@ -8,10 +9,11 @@ import javax.validation.constraints.Size;
  * Created by kmichalik on 7/12/2016.
  */
 public class LoginViewModel {
-    @NotEmpty
+    @NotEmpty(message = "E-mail is empty.")
+    @Email(message = "E-mail is invalid.")
     public String email;
 
-    @NotEmpty
+    @NotEmpty(message = "Password is empty.")
     public String password;
 
     public String getEmail() {
