a22075240ad12ca7abcd0126f5016a6fc9d2a281
Kamil Michalik (PGS Software) <kmichalik@pgs-soft.com>
2016-07-15 13:26:30 +0200

    PN-6 / PN-26 As a project member I can report my feelings in a project for every day
    *Add method checkDayMood.

--- src/main/java/com/pgs/intern/dao/MoodDao.java
+++ src/main/java/com/pgs/intern/dao/MoodDao.java
@@ -2,12 +2,16 @@ package com.pgs.intern.dao;
 
 import com.pgs.intern.models.Mood;
 import com.pgs.intern.models.Project;
+import com.pgs.intern.models.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by mzalucka on 7/14/2016.
@@ -29,5 +33,19 @@ public class MoodDao {
 
     public void save(Mood mood) {
         entityManager.persist(mood);
+    }
+
+    public boolean checkDayMood(Date dateAdd, User user, Project project) {
+        String queryString = "SELECT count(o.dateAdd) FROM Mood o where (u.idUser) =:idUser and (p.projectId)=:projectId and (o.dateAdd) = :dateAdd";
+        Query query = getEntityManager().createQuery(queryString);
+        query.setParameter("dateAdd", dateAdd);
+        query.setParameter("user", user.getIdUser());
+        query.setParameter("project", project.getProjectId());
+
+        List result = query.getResultList();
+        long firstElement = (long) result.get(0);
+        System.out.println(firstElement);
+
+        return (firstElement > 0);
     }
 }
