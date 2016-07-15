782c6d9f69ef2f06b2555f9aa504cce7bf623bb9
Kamil Michalik (PGS Software) <kmichalik@pgs-soft.com>
2016-07-15 09:31:02 +0200

    PN-10 / PN-15 As a anonymous user I can register in application
    *Change adnotation to validate fields.

--- src/main/java/com/pgs/intern/models/RegistrationViewModel.java
+++ src/main/java/com/pgs/intern/models/RegistrationViewModel.java
@@ -1,6 +1,7 @@
 package com.pgs.intern.models;
 
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Pattern;
@@ -11,11 +12,11 @@ import javax.validation.constraints.Size;
  */
 public class RegistrationViewModel {
 
-    @NotEmpty(message = "{NotEmpty.message}")
+    @NotBlank(message = "{NotEmpty.message}")
     @Size(max = 32, message = "{Size.displayName}")
     private String displayName;
 
-    @NotEmpty(message = "{NotEmpty.message}")
+
     @Pattern(regexp="^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message="{Email.invalidEmail}")
     private String email;
 
