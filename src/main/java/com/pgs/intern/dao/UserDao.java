b85096b2202843a5d6288c34ee174a4cac3b344a
Kamil Michalik (PGS Software) <kmichalik@pgs-soft.com>
2016-07-18 16:18:10 +0200

    PN-10/ PN-14 As a anonymous user I can register in application
    *Changed query to TypedQuery in method checkByEmail.
     *Changed query to TypedQuery in method findUser.
     *Modified method findUser.

--- src/main/java/com/pgs/intern/dao/UserDao.java
+++ src/main/java/com/pgs/intern/dao/UserDao.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 /**
@@ -27,27 +28,22 @@ public class UserDao {
 
     public boolean checkByEmail(String email) {
         String queryString = "SELECT count(o.email) FROM User o where LOWER(o.email) = :email";
-        Query query = entityManager.createQuery(queryString);
+        TypedQuery<Long> query = entityManager.createQuery(queryString, Long.class);
         query.setParameter("email", email.toLowerCase());
 
-        List result = query.getResultList();
-        long firstElement = (long) result.get(0);
-
-        return (firstElement > 0);
+        return (query.getSingleResult() > 0);
     }
 
     public User findUser(String email) {
-        User result = null;
         String queryString = "Select o from User o where LOWER(o.email) = :email ";
-
-        Query query = entityManager.createQuery(queryString);
+        TypedQuery<User> query = entityManager.createQuery(queryString, User.class);
         query.setParameter("email", email.toLowerCase());
         try {
-            result = (User) query.getSingleResult();
+            return query.getSingleResult();
         } catch (Exception e) {
             return null;
         }
-        return result;
+
     }
 
     public User findById(Long id) {
