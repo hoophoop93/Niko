7f56f40a7f551c57cc034abf67cca0905edd9164
Monika Zalucka (PGS Software) <mzalucka@pgs-soft.com>
2016-07-26 14:41:34 +0200

    Added getUserProjects and getNoneJoinedUsersById methods to new Project Repository.

--- src/main/java/com/pgs/intern/dao/ProjectDaoJpa.java
+++ src/main/java/com/pgs/intern/dao/ProjectDaoJpa.java
@@ -14,11 +14,17 @@ import java.util.List;
  */
 
 public interface ProjectDaoJpa extends CrudRepository<Project,Long> {
+
     Project findByTitle(String title);
+
+    List<Project> findByOwnerOrderByTitleAsc(User owner);
 
     @Query("SELECT count(o.title) > 0 FROM Project o where LOWER(o.title) =  LOWER(:title)")
     boolean existsByTitle(@Param("title") String title);
 
-    List<Project> findByOwnerOrderByTitleAsc(User owner);
+    @Query("SELECT p FROM Project p, User u WHERE u = :user AND (u = p.owner OR p in elements(u.joinedProjectsList)) ORDER BY p.title ASC")
+    List<Project> getUserProjects(@Param("user") User user);
 
+    @Query("SELECT u FROM User u, Project p WHERE p.projectId = :projectId AND u not in elements( p.joinedUsersList ) and u <> p.owner")
+    List<User> getNoneJoinedUsersById(@Param("projectId") Long projectId);
 }
