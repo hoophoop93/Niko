a05a325020001571a939cff109912ebda687ac42
Monika Zalucka (PGS Software) <mzalucka@pgs-soft.com>
2016-07-27 10:19:20 +0200

    PN-7 / PN-67 As a project member I can see what feelings were reported by other users in the project
    *Modified findAllMoodsInProjectAndDate in MoodDao.

--- src/main/java/com/pgs/intern/dao/MoodDaoDataJpaInterface.java
+++ src/main/java/com/pgs/intern/dao/MoodDaoDataJpaInterface.java
@@ -16,6 +16,6 @@ public interface MoodDaoDataJpaInterface extends CrudRepository<Mood, Long> {
     @Query("select count(mood.dateAdd) > 0 from Mood mood where mood.user = ?1 and mood.project = ?2 and mood.dateAdd = ?3")
     boolean checkDayMood(User user, Project project, Date dateAdd);
 
-    @Query("select mood from Mood mood where mood.project = ?1 and mood.dateAdd = ?2")
-    List<Mood> findAllMoodsInProjectAndDate(Project project,Date date);
+    @Query("select mood from Mood mood where mood.project = ?1 and mood.dateAdd between ?2 and ?3")
+    List<Mood> findAllMoodsInProjectAndDate(Project project, Date minDate, Date maxDate);
 }
