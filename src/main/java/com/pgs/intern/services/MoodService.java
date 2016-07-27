24740c03a5b69409cf469a6d498e03142036c7ef
Maciej Rosa (PGS Software) <mrosa@pgs-soft.com>
2016-07-27 12:21:28 +0200

    PN-7 / PN-69 As a project member I can see what feelings were reported by other users in the project
    * created function which returns MoodsViewController.

--- src/main/java/com/pgs/intern/services/MoodService.java
+++ src/main/java/com/pgs/intern/services/MoodService.java
@@ -1,15 +1,26 @@
 package com.pgs.intern.services;
 
 import com.pgs.intern.dao.MoodDaoDataJpaInterface;
+import com.pgs.intern.dao.ProjectDaoJpa;
 import com.pgs.intern.models.Mood;
 import com.pgs.intern.models.MoodViewModel;
+import com.pgs.intern.models.Project;
+import com.pgs.intern.models.moods.view.DailyMoodReport;
+import com.pgs.intern.models.moods.view.MoodReport;
+import com.pgs.intern.models.moods.view.MoodsViewModel;
+import com.pgs.intern.models.moods.view.ProjectMoodsReport;
 import org.apache.commons.lang3.time.DateUtils;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Maciej Rosa on 7/15/2016 12:53 PM.
@@ -18,6 +29,12 @@ import java.util.Date;
 public class MoodService {
     @Autowired
     private MoodDaoDataJpaInterface moodDao;
+
+    public final int DAYS = 8;
+
+
+    @Autowired
+    private ProjectDaoJpa projectDaoJpa;
 
     @Inject
     private CurrentUser currentUser;
@@ -36,4 +53,45 @@ public class MoodService {
         return sevenDaysAgo.compareTo(DateUtils.truncate(date,Calendar.DATE)) <= 0;
     }
 
+    private int getDaysAgoCount(Date date){
+        DateTime dateTime = new DateTime(date);
+        return Math.abs(Days.daysBetween(dateTime,new DateTime()).getDays());
+    }
+
+
+    public MoodsViewModel getMoodsViewModel(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date today = DateUtils.truncate(new Date(),Calendar.DATE);
+        Date daysAgo = DateUtils.addDays(today,-7);
+
+
+        List<ProjectMoodsReport> projectMoodsReports = new ArrayList<>();
+        for(Project p : projectDaoJpa.getUserProjects(currentUser.getUser())){
+            ProjectMoodsReport report = new ProjectMoodsReport();
+            report.setTitle(p.getTitle());
+            report.setOwner(p.getOwner().getDisplayName());
+            DailyMoodReport[] dailyReports = new DailyMoodReport[DAYS];
+
+            for(int i = 0; i < DAYS; i++){
+                dailyReports[i] = new DailyMoodReport();
+                dailyReports[i].setMoodReports(new ArrayList<>());
+                dailyReports[i].setDate(simpleDateFormat.format(DateUtils.addDays(today,i-DAYS+1)));
+                dailyReports[i].setWeekend(false);
+            }
+            for(Mood mood : moodDao.findAllMoodsInProjectAndDate(p,daysAgo,today)){
+                MoodReport moodReport = new MoodReport();
+                moodReport.setMood(mood.getMoodType().toString());
+                moodReport.setDisplayName(mood.getUser().getDisplayName());
+                int day = DAYS - getDaysAgoCount(mood.getDateAdd()) - 1;
+                if(day >= 0)
+                    dailyReports[day].getMoodReports().add(moodReport);
+            }
+            report.setDailyMoodReports(dailyReports);
+            projectMoodsReports.add(report);
+        }
+        MoodsViewModel moodsViewModel = new MoodsViewModel();
+        moodsViewModel.setProjectMoodsReportList(projectMoodsReports);
+        return moodsViewModel;
+    }
 }
